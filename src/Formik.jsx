e62d20097626dd23045c5fb52048bf28863c2ab7
Sudheer Chakka <chsudheer08@gmail.com>
2023-09-17 15:23:59 +0530

    password updated

--- src/Formik.jsx
+++ src/Formik.jsx
@@ -9,18 +9,23 @@ function Formik() {
       firstName: '',
       lastName: '',
       email: '',
+      password:'',
     },
     validationSchema: Yup.object({
       firstName: Yup.string()
-        .max(15, 'Must be 15 characters or less')
+        .min(15, 'Must be 15 characters or less')
         .required('Must be 15 characters or less'),
       lastName: Yup.string()
         .max(20, 'Must be 20 characters or less')
         .required('Required'),
       email: Yup.string().email('Invalid email address').required('Required'),
+      password:Yup.string().required('Password is required')
+      .min(8, 'Password must be at least 8 characters')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).*$/,'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@#$%^&+=)')
     }),
     onSubmit: values => {
-      // alert(JSON.stringify(values, null, 4));
+      alert(JSON.stringify(values, null, 4));
       console.log(values.email);
     },
   });
@@ -61,8 +66,17 @@ function Formik() {
          onChange={formik.handleChange}
          value={formik.values.email}
        />
-       {formik.touched.email && formik.errors.email ? (
-         <div>{formik.errors.email}</div>
+       <label htmlFor="password">Password</label>
+       <input
+         id="password"
+         name="password"
+         type="text"
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.password}
+       />
+       {formik.touched.password && formik.errors.password ? (
+         <div>{formik.errors.password}</div>
        ) : null}
        <button type="submit">Submit</button>
      </form>
